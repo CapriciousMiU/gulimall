90520815ce01f814195971f2457364344b431a71
Miu <hax16772785250@163.com>
2024-03-21 16:43:27 +0800

    第一次修改

---
+++ gulimall-ware/src/test/java/com/atguigu/cpm/gulimallware/GulimallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.cpm.gulimallware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
